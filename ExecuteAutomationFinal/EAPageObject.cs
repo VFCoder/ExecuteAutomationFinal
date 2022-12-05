abf6ecaca733c4072b3dbb823d0e56fd35d60c73
VFCoder <vincefield17@hotmail.com>
2022-12-05 20:13:10 -0300

    added page objects core package

---
+++ ExecuteAutomationFinal/EAPageObject.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteAutomationFinal
+{
+    class EAPageObject      //create page object properties for easy coding
+    {
+
+        public EAPageObject()
+        {
+            PageFactory.InitElements(PropertiesCollection.driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "TitleId")]
+        public IWebElement ddTitleId { get; set; }
+
+        [FindsBy(How = How.Name, Using = "Initial")]
+        public IWebElement txtInitial { get; set; }
+
+        [FindsBy(How = How.Name, Using = "Save")]
+        public IWebElement btnSave { get; set; }
+
+    }
+}
