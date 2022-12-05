a4a720364864ff45ccee95e82102633ca7ff0909
VFCoder <vincefield17@hotmail.com>
2022-12-04 21:57:16 -0300

    Add project files.

---
+++ ExecuteAutomationFinal/Program.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteAutomationFinal //custom methods to set values (text box, drop down menu, check box)
+{
+    class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        static void Main(string[] args)
+        {
+        }
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://demosite.executeautomation.com/index.html");
+
+            Console.WriteLine("Opened URL");
+        }
+        [Test]
+        public void ExecuteTest()
+        {
+            //title
+            SeleniumSetMethods.SelectDropDown(driver, "TitleId", "Mr.", "Id");
+
+            //initial
+            SeleniumSetMethods.EnterText(driver, "Initial", "UserText.", "Name");
+
+            //click
+            SeleniumSetMethods.Click(driver, "Save", "Name");
+
+            Console.WriteLine("Executed Test");
+
+
+        }
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+
+            Console.WriteLine("Closed browser");
+        }
+    }
+}
---
+++ ExecuteAutomationFinal/SeleniumSetMethods.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteAutomationFinal
+{
+    class SeleniumSetMethods
+    {
+
+        //  custom method to enter text:
+        //  EnterText(element (text box), value (text to input), type (Id, Name, etc.))
+        public static void EnterText(IWebDriver driver, string element, string value, string elementType)
+        {
+           if (elementType == "Id")
+            driver.FindElement(By.Id(element)).SendKeys(value);
+
+            if (elementType == "Name") 
+                driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+        //  click button method
+        public static void Click(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+                driver.FindElement(By.Id(element)).Click();
+
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+
+        //  select drop down menu
+        public static void SelectDropDown(IWebDriver driver, string element, string value, string elementType)
+        {
+
+            if (elementType == "Id")
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+
+            if (elementType == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+        }
+    }
+}
